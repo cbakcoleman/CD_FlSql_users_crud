e675042894740f2867c6aa80c51188a344f75efd
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-09-09 18:09:01 -0700

    add view all users, create new user, delete user

--- server.py
+++ server.py
@@ -4,7 +4,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("create_user.html")
+
+@app.route("/users_all")
+def users_all():
+    users_all = User.get_all()
+    print(users_all)
+    return render_template("users_all.html", users_all = users_all)
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = {
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+    new_user = User.create_user(data)
+    return redirect("/users_all")
+
+@app.route("/user_ind/<int:id>")
+def user_ind(id):
+    data = {
+        "id" : id
+    }
+    user_ind = User.get_ind(data)
+    print(user_ind)
+    return render_template("user_ind.html", user = user_ind)
+
+
+@app.route("/delete_user/<int:id>")
+def delete_user(id):
+    data = {
+        "id" :  id
+    }
+    deleted_user = User.delete_user(data)
+    return redirect("/users_all")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,40 @@
+from mysqlconnection import connectToMySQL
+
+class User:
+    def __init__( self, data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name= data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL("users_schema").query_db( query )
+        users_all = []
+        for row in results:
+            users_all.append( cls(row) )
+        return users_all
+
+    @classmethod
+    def create_user(cls, data):
+        query = "INSERT INTO users ( first_name, last_name, email ) VALUES ( %(first_name)s, %(last_name)s, %(email)s );"
+        new_user = connectToMySQL("users_schema").query_db( query, data )
+        return new_user
+
+    @classmethod
+    def get_ind(cls, id):
+        query = "SELECT * FROM users WHERE ID = %(id)s ;"
+        ind_users = connectToMySQL("users_schema").query_db( query, id )
+        user_ind = []
+        for row in ind_users:
+            user_ind.append( cls(row) )
+        return user_ind
+
+    @classmethod
+    def delete_user(cls, id):
+        query = "DELETE FROM users WHERE id = %(id)s ;"
+        deleted_user = connectToMySQL("users_schema").query_db( query, id )
+        return deleted_user
